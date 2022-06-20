299311c81542805d1b0bed617aa40c23c76a6315
alaaahmeedd <alaa.aahmeedd@gmail.com>
2022-06-20 12:46:11 +0200

    add path_image in employee model

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -132,7 +132,7 @@ class AuthController extends Controller
         $empofdepartment = DB::table('departments')
             ->join('employees','employees.department_id', '=' ,'departments.id')
             ->where('email', $fields['email'])
-            ->select('departments.*', 'employees.department_id','employees.id' ,'employees.name','employees.position')
+            ->select('departments.*', 'employees.department_id','employees.id' ,'employees.name','employees.position','employees.path_image')
             ->first(); 
 
    
