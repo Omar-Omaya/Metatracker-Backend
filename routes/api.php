60998847e3d8b11f89da1fc323a0f43bd8e2f910
alaaahmeedd <alaa.aahmeedd@gmail.com>
2022-09-12 16:35:58 +0200

    edit get All Message employee

--- routes/api.php
+++ routes/api.php
@@ -121,7 +121,7 @@ Route::get('/msg', [MessageController::class , 'index']);
 
 Route::post('/msgemp/{id}', [MessageController::class , 'messageEmployee']);
 Route::get('/getmsgemp', [MessageController::class , 'getMessageEmpMobile']);
-Route::get('/getmsgemp', [MessageController::class , 'getAllMessageEmp']);
+Route::get('/getallmsgemp', [MessageController::class , 'getAllMessageEmp']);
 
 
 Route::post('/msgdep/{id}', [MessageController::class , 'messageDepartment']);
